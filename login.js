d42a39c35890cdadbfe05adf479744eedb964141
TopiaYu <raemo963@naver.com>
2025-04-01 13:22:02 +0900

    Add signup.js, add button disabled

--- login.js
+++ login.js
@@ -10,7 +10,9 @@ const validEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-
 document.getElementById('email').autocomplete = 'off';
 document.getElementById('password').autocomplete = 'off';
 
-console.log(userEmailField);
+//버튼 유효성 검사
+let isEmailValid = false;
+let isPasswordValid = false;
 
 function handleFormEmail(event) {
     const input = userEmailField.value;
@@ -18,15 +20,19 @@ function handleFormEmail(event) {
         emailErrorMessage.textContent = "이메일을 입력해주세요";
         emailErrorMessage.style.color = 'red';
         userEmailField.style.border = '1px solid red';
+        isEmailValid = false;
     } else if (validEmail.test(input) ) {
         emailErrorMessage.textContent = "올바른 이메일 형식입니다";
         emailErrorMessage.style.color = '#3692FF';
         userEmailField.style.border = '1px solid #3692FF';
+        isEmailValid = true;
     } else {
         emailErrorMessage.textContent = "잘못된 이메일 형식입니다";
         emailErrorMessage.style.color = 'red';
         userEmailField.style.border = '1px solid red';
+        isEmailValid = false;
     }
+    handleLoginButton();
 }
 
 function handleFormPassword(e) {
@@ -34,23 +40,29 @@ function handleFormPassword(e) {
     if ( input === '' ) {
         passwordErrorMessage.textContent = "비밀번호를 입력해주세요";  
         passwordErrorMessage.style.color = 'red';
-        userPasswordField.style.border = '1px solid red';  
+        userPasswordField.style.border = '1px solid red';
+        isPasswordValid = false;
     } else if ( input.length < 8 ) {
         passwordErrorMessage.textContent = "비밀번호를 8자 이상 입력해주세요";
         passwordErrorMessage.style.color = 'red';
         userPasswordField.style.border = '1px solid red';  
+        isPasswordValid = false;
     } else {
         passwordErrorMessage.textContent = "유효한 비밀번호 입니다"
         userPasswordField.style.border = '1px solid #3692FF';
         passwordErrorMessage.style.color = ' #3692FF';
-        loginButton.style.backgroundColor = '#3692FF';
+        isPasswordValid = true;
     }
+    handleLoginButton();
 }
 
 userEmailField.addEventListener('focusout', handleFormEmail);
 userPasswordField.addEventListener('focusout', handleFormPassword);
 
-
+//버튼 활성화 함수
 function handleLoginButton(event) {
-    
+    if ( isEmailValid && isPasswordValid ) {
+        loginButton.style.backgroundColor = '#3692FF';
+        loginButton.disabled = false;
+    }
 }
