130b596368650a744c8b5daf462b0d62d98ebf7f
TopiaYu <raemo963@naver.com>
2025-05-09 23:52:32 +0900

    데스크탑 UI 구현

--- src/components/pages/AddItem.jsx
+++ src/components/pages/AddItem.jsx
@@ -1,6 +1,124 @@
+import { css } from '@emotion/react';
+import plusIcon from '../../assets/plusIcon.png'
+
+/** @jsxImportSource @emotion/react */
+
+const itemWrapper = css`
+
+    > div {
+        margin: 32px 0;
+    }
+
+    > div > input, textarea {
+        border-radius: 12px;
+        width: 100%;
+        height: 56px;
+        background: var(--gray-100);
+        margin-top: 16px;
+        padding-left: 20px;
+        border: none;
+        cursor: pointer;
+    }
+
+    > div > textarea {
+        height: 282px;
+        padding-top: 15px;
+    }
+`
+
+const addItem = css`
+    max-width: 1200px;
+    // background: pink;
+    margin: 0 auto;
+`
+const contentHeader = css`
+    width: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 24px;
+    > button {
+        border-radius: 8px;
+        border: none;
+        width: 74px;
+        height: 42px;
+    }
+`
+
+const headerButton = css`
+
+`
+
+const addItemImageWrapper = css`
+`
+
+const addItemBox = css`
+    margin-top: 16px;    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+    width: 282px;
+    aspect-ratio: 1/1;
+    background: var(--gray-100);
+    border-radius: 12px;
+`
+
+const plusIconStyle = css`
+    width: 48px;
+    aspect-ratio: 1/1;
+`
 
 const AddItem = () => {
-    return <div>AddItem</div>;
+
+    return (
+        <>
+            <form css={addItem}>
+                <div css={contentHeader}>
+                    <h3>상품 등록하기</h3>
+                    <button type="submit" css={headerButton}>등록</button>
+                </div>
+                <div css={itemWrapper}>
+                    <div >
+                        <p>상품 이미지</p>
+                        <label css={addItemImageWrapper}>
+                            <div css={addItemBox}>
+                                <img src={plusIcon} alt='추가' css={plusIconStyle} />
+                                <span>이미지 등록</span>
+                            </div>
+                        </label>
+                        <input  
+                            id='fileUpload'
+                            type='file'
+                            accept='image/*'
+                            style={{display: 'none'}}
+                        />    
+                    </div>
+
+                    <div>
+                        <label htmlFor='name'>상품명</label>
+                        <input id="name" placeholder="상품명을 입력해주세요"></input>
+                    </div>
+                    
+                    <div>
+                        <label>상품 소개</label>
+                        <textarea id="description" placeholder="상품 소개를 입력해주세요"></textarea>
+                    </div>
+
+                    <div>
+                        <label>판매가격</label>
+                        <input id="price" placeholder="판매가격을 입력해주세요"></input>
+                    </div>
+
+                    <div>
+                        <label>태그</label>
+                        <input id="tag" placeholder="태그를 입력해주세요"></input>
+                    </div>
+                </div>
+            </form>
+        </>
+    );
 };
 
 export default AddItem;
