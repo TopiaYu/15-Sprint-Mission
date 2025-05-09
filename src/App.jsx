1a66cd3dfd2d6c5a0ea702d47d69d552bbfd9c6c
TopiaYu <raemo963@naver.com>
2025-05-09 11:31:45 +0900

    Items 경로수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { globalStyle } from './GlobalStyle';
 import Header from './components/Header';
 import Main from './components/pages/Main';
 import FreeBoard from './components/pages/FreeBoard';
-import Items from "./components/pages/items";
+import Items from './components/pages/Items';
 import { Route, Routes } from "react-router";
 import AddItem from './components/pages/AddItem';
 
